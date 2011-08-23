284757bcf815774a5ad92a9b0077c89bdd6b11a8
Giuliano Vilela <giulianoxt@gmail.com>
2011-08-23 12:50:49 -0300

    List processos ativo

--- src/projudi.py
+++ src/projudi.py
@@ -3,6 +3,7 @@ from BeautifulSoup import BeautifulSoup
 
 
 LOGIN_URL = 'https://projudi.tjrn.jus.br/projudi/indexParte.jsp'
+MAIN_PAGE_URL = 'https://projudi.tjrn.jus.br/projudi/parte/frameCentroParte.jsp'
 
 
 def init_session():
@@ -26,5 +27,28 @@ def login(browser, cpf, password):
   form.set_value(cpf, id='cpfCnpj', nr=0)
   form.set_value(password, id='senha')
 
-  response = browser.submit()
-  return response
+  return browser.submit()
+
+
+def _goto_processos_ativo(browser):
+  return browser.follow_link(url_regex='ativosPoloAtivo')
+
+def _goto_main_page(browser):
+  return browser.open(MAIN_PAGE_URL)
+
+
+def get_processos_ativo(browser):
+  res = _goto_processos_ativo(browser)
+
+  soup = BeautifulSoup(res.read())
+  table_procs = soup.find(id='texto').table
+
+  processos = []
+
+  for row_proc in table_procs.findAll('tr')[2:]:
+    td = row_proc.findAll('td')[1]
+    proc_str = td.a.text.strip()
+    processos.append(str(proc_str))
+
+  _goto_main_page(browser)
+  return processos
